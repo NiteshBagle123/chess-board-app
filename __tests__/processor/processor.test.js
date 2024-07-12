73b77242441895e4ec0592efa4cfcbed7332c354
Nitesh Bagle <niteshbagle43@gmail.com>
2024-07-12 15:56:50 +0530

    test cases refinement

--- __tests__/processor/processor.test.js
+++ __tests__/processor/processor.test.js
@@ -1,29 +1,66 @@
 const { chessProcessor } = require('../../processor');
-const {
-  INPUT_VALIDATOR_FAILURE,
-  PAWN_CHESS_PIECE: { INPUT, OUTPUT },
-  KING_CHESS_PIECE: { INPUT: KING_INPUT, OUTPUT: KING_OUTPUT },
-  QUEEN_CHESS_PIECE: { INPUT: QUEEN_INPUT, OUTPUT: QUEEN_OUTPUT }
-} = require('./fixtures/fixtures');
 
+// eslint-disable-next-line no-undef
 describe('Should test chess processor flow', () => {
+  // eslint-disable-next-line no-undef
   it('Should test input validator failure', () => {
+    const INPUT_VALIDATOR_FAILURE = {
+      // eslint-disable-next-line max-len
+      message: 'Please provide input in terminal with format node <file_name> <type> <position> for example: nodejs index.js pawn A1',
+      status: 'failure'
+    };
     const chessProcessorResponse = chessProcessor([]);
+    // eslint-disable-next-line no-undef
     expect(chessProcessorResponse).toEqual(INPUT_VALIDATOR_FAILURE);
   });
 
+
+  // eslint-disable-next-line no-undef
   it('Should test for chess piece type pawn and postion A1', () => {
-    const chessProcessorResponse = chessProcessor(INPUT);
-    expect(chessProcessorResponse).toEqual(OUTPUT);
+    const PAWN_CHESS_PIECE = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'pawn',
+        'A1'
+      ],
+      OUTPUT: 'A2'
+    };
+    const chessProcessorResponse = chessProcessor(PAWN_CHESS_PIECE.INPUT);
+    // eslint-disable-next-line no-undef
+    expect(chessProcessorResponse).toEqual(PAWN_CHESS_PIECE.OUTPUT);
   });
 
+  // eslint-disable-next-line no-undef
   it('Should test for chess piece type king and postion D5', () => {
-    const chessProcessorResponse = chessProcessor(KING_INPUT);
-    expect(chessProcessorResponse).toEqual(KING_OUTPUT);
+    const KING_CHESS_PIECE = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'king',
+        'D5'
+      ],
+      OUTPUT: ['C4', 'C5', 'C6', 'D4', 'D6', 'E4', 'E5', 'E6']
+    };
+    const chessProcessorResponse = chessProcessor(KING_CHESS_PIECE.INPUT);
+    // eslint-disable-next-line no-undef
+    expect(chessProcessorResponse).toEqual(KING_CHESS_PIECE.OUTPUT);
   });
 
+  // eslint-disable-next-line no-undef
   it('Should test for chess piece type queen and postion E4', () => {
-    const chessProcessorResponse = chessProcessor(QUEEN_INPUT);
-    expect(chessProcessorResponse).toEqual(QUEEN_OUTPUT);
+    const QUEEN_CHESS_PIECE = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'queen',
+        'E4'
+      ],
+      // eslint-disable-next-line max-len
+      OUTPUT: ['A4', 'B4', 'C4', 'D4', 'F4', 'G4', 'H4', 'E1', 'E2', 'E3', 'E5', 'E6', 'E7', 'E8', 'A8', 'B7', 'C6', 'D5', 'F3', 'G2', 'H1', 'B1', 'C2', 'D3', 'F5', 'G6', 'H7']
+    };
+    const chessProcessorResponse = chessProcessor(QUEEN_CHESS_PIECE.INPUT);
+    // eslint-disable-next-line no-undef
+    expect(chessProcessorResponse).toEqual(QUEEN_CHESS_PIECE.OUTPUT);
   });
 });
--- __tests__/validator/input-validator.test.js
+++ __tests__/validator/input-validator.test.js
@@ -1,16 +1,20 @@
 const { inputValidator } = require('../../validator');
-const {
-  INPUT_MISSING,
-  INPUT_INVALID,
-  CHESS_POSITION_INVALID,
-  CHESS_START_POSITION_INVALID,
-  CHESS_END_POSITION_INVALID
-} = require('./fixtures/fixtures');
 
 // eslint-disable-next-line no-undef
 describe('Should test input validator flows', () => {
   // eslint-disable-next-line no-undef
   it('Should test when input is missing like chess piece type or position', () => {
+    const INPUT_MISSING = {
+      INPUT: [
+        'node_executable',
+        'js file path'
+      ],
+      OUTPUT: {
+        // eslint-disable-next-line max-len
+        message: 'Please provide input in terminal with format node <file_name> <type> <position> for example: nodejs index.js pawn A1',
+        status: 'failure'
+      }
+    };
     const inputValidatorResponse = inputValidator(INPUT_MISSING.INPUT);
     // eslint-disable-next-line no-undef
     expect(inputValidatorResponse).toEqual(INPUT_MISSING.OUTPUT);
@@ -18,6 +22,18 @@ describe('Should test input validator flows', () => {
 
   // eslint-disable-next-line no-undef
   it('Should test when input chess position type or position is invalid', () => {
+    const INPUT_INVALID = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'test',
+        'test1'
+      ],
+      OUTPUT: {
+        message: 'Please provide valid chess piece type like pawn, king or queen',
+        status: 'failure'
+      }
+    };
     const inputValidatorResponse = inputValidator(INPUT_INVALID.INPUT);
     // eslint-disable-next-line no-undef
     expect(inputValidatorResponse).toEqual(INPUT_INVALID.OUTPUT);
@@ -25,6 +41,19 @@ describe('Should test input validator flows', () => {
 
   // eslint-disable-next-line no-undef
   it('Should test when input chess position is invalid', () => {
+    const CHESS_POSITION_INVALID = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'pawn',
+        'a'
+      ],
+      OUTPUT: {
+        // eslint-disable-next-line max-len
+        message: 'Chess position: a value is invalid. Please provide valid chess position which starts from (A, B, ...H)(1,2 ...8) for example A1, H8',
+        status: 'failure'
+      }
+    };
     const inputValidatorResponse = inputValidator(CHESS_POSITION_INVALID.INPUT);
     // eslint-disable-next-line no-undef
     expect(inputValidatorResponse).toEqual(CHESS_POSITION_INVALID.OUTPUT);
@@ -32,6 +61,19 @@ describe('Should test input validator flows', () => {
 
   // eslint-disable-next-line no-undef
   it('Should test when input chess start position is invalid', () => {
+    const CHESS_START_POSITION_INVALID = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'pawn',
+        'x1'
+      ],
+      OUTPUT: {
+        // eslint-disable-next-line max-len
+        message: 'Chess position: x1 is invalid. It should starts with A, B, ..H as chessboard is an 8 x 8 grid with 64 cells in it',
+        status: 'failure'
+      }
+    };
     const inputValidatorResponse = inputValidator(CHESS_START_POSITION_INVALID.INPUT);
     // eslint-disable-next-line no-undef
     expect(inputValidatorResponse).toEqual(CHESS_START_POSITION_INVALID.OUTPUT);
@@ -39,6 +81,19 @@ describe('Should test input validator flows', () => {
 
   // eslint-disable-next-line no-undef
   it('Should test when input chess end position is invalid', () => {
+    const CHESS_END_POSITION_INVALID = {
+      INPUT: [
+        'node_executable',
+        'js file path',
+        'pawn',
+        'A9'
+      ],
+      OUTPUT: {
+        // eslint-disable-next-line max-len
+        message: 'Chess position: A9 is invalid. It should ends with 1, 2, ..8 as chessboard is an 8 x 8 grid with 64 cells in it',
+        status: 'failure'
+      }
+    };
     const inputValidatorResponse = inputValidator(CHESS_END_POSITION_INVALID.INPUT);
     // eslint-disable-next-line no-undef
     expect(inputValidatorResponse).toEqual(CHESS_END_POSITION_INVALID.OUTPUT);
